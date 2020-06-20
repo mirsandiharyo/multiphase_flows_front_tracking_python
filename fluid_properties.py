4669854ce13124c239e6f2f0952b5b6f59b588aa
mirsandiharyo <mirsandiharyo@gmail.com>
2020-06-20 20:14:53 +0200

    Added a class for fluid properties

---
+++ fluid_properties.py
@@ -0,0 +1,12 @@
+"""
+FluidProp class
+Created on Sat Jun 20 19:55:00 2020
+
+@author: mirsandiharyo
+"""
+
+class FluidProp:
+    def __init__(self, rho, mu, sigma):     
+        self.rho = rho
+        self.mu = mu
+        self.sigma = sigma
