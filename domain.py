a6f335f25ed3f7dab491963076c3edf1a644cadb
mirsandiharyo <mirsandiharyo@gmail.com>
2020-06-20 19:48:08 +0200

    Added Domain class

---
+++ domain.py
@@ -0,0 +1,15 @@
+"""
+Domain class
+Created on Sat Jun 20 19:45:03 2020
+
+@author: mirsandiharyo
+"""
+
+class Domain:
+    def __init__(self, lx, ly, nx, ny, gravx, gravy):
+        self.lx = lx
+        self.ly = ly
+        self.nx = nx
+        self.ny = ny
+        self.gravx = gravx
+        self.gravy = gravy
