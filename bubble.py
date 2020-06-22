733cf0eeede74c71b2afa9cb62b19f22baddf746
mirsandiharyo <mirsandiharyo@gmail.com>
2020-06-22 14:53:30 +0200

    Removed bugs

--- bubble.py
+++ bubble.py
@@ -10,6 +10,7 @@ import math
 
 class Bubble:
     total = 0
+    
     def __init__(self, center_x, center_y, radius, point): 
         self.center_x = center_x
         self.center_y = center_y
@@ -23,6 +24,6 @@ class Bubble:
         
     def initialize_front(self):
         # determine the location of the initial spherical bubble
-        for i in range(1, self.point+2):
-            self.x[i] = self.center_x-self.radius*math.sin(2.0*math.pi*(i-1)/self.point)
-            self.y[i] = self.center_y-self.radius*math.cos(2.0*math.pi*(i-1)/self.point)
+        for i in range(self.point+2):
+            self.x[i] = self.center_x-self.radius*math.sin(2.0*math.pi*i/self.point)
+            self.y[i] = self.center_y-self.radius*math.cos(2.0*math.pi*i/self.point)
